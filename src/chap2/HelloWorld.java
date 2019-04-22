e19ac612b26dc6d54b7ef9d56593fb44ce27d01e
gdj13 <gdj13@DESKTOP-D53NO4M>
2019-04-22 17:56:46 +0900

    업업로드 테스트

---
+++ src/chap2/HelloWorld.java
@@ -0,0 +1,34 @@
+package chap2;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		/*System.out.println("변수시작");
+		
+		String 문자열 = "HelloWorld ";
+		System.out.println(문자열);
+		문자열 ="문자열변경";
+		System.out.println(문자열);
+		문자열 = "1234";
+		System.out.println(문자열);
+		double 실수 =1.23234456777777778866555;
+		int 정수 =1;
+		System.out.println(정수);
+		
+		double 결과값 = 실수+정수+정수+정수+실수;
+		System.out.println(결과값);
+		float f = 0.1f;
+		double d = 10.5; 
+		System.out.println("f=" +f);
+		System.out.println("d="+d);
+		byte b1 = 10;
+		short s1 = 20;
+		int i1 = 100;
+		long l1= 1000;
+		i1 = b1;
+		s1 = (short)l1;
+		float f =l1;*/
+		char single = '\'';
+		String quote = "Hello\"홍길동!\"";
+		
+	}
+}
